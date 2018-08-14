af89d0aff05a204e9ac4e4c8698ff4545ae8cf24
CrimsonFury82 <1007626@student.sae.edu.au>
2018-08-14 17:42:28 +0800

    Aston

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //allows loading of scenes
+
+//script written by Aston Olsen
+
+public class MenuController : MonoBehaviour {
+
+	public void LoadSceneDelayed(string sceneName){ //function for loading scenes with a delay
+		StartCoroutine("Load", sceneName); //Calls coroutine
+	}
+
+	IEnumerator Load(string sceneName) { //coroutine for loading a scene
+		yield return new WaitForSeconds (0.3f); //waits for 1 second
+		SceneManager.LoadScene (sceneName); //Loads the assigned scene when this function is run
+	}
+
+	public void QuitDelayed(){ //function quit quitting with delay
+		Invoke ("Quit", 0.3f); //calls function with specified delay
+	}
+
+	public void Quit(){ //function for quitting the game when Quit button is clicked
+		print("Quitting game"); //"Displays "Quitting game" to console
+		Application.Quit(); //Quits the application (Built game only, doesn't work within Unity editor)
+	}
+
+}
+
+
+
+
+
+
+
+
